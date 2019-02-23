4a3aa37a3197f44d5c6bf4efa2fbfe8840aff907
sbx_user1060 <sbx_user1060@ip-10-129-10-165.eu-west-2.compute.internal>
2019-02-23 12:50:34 +0000

    Sat Feb 23 12:50:34 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,6 +3,14 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return "Hello, World!"
+#    if type(friend_name) == type(str()):
+#        raise TypeError
+    return "Hello, " + friend_name + "!"
+
+
+hello("Matto")
+
+#hello(1)
+
 
 
