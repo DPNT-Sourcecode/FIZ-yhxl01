cb4c68a2e8e9d89aafb4c838e681e88070708651
sbx_user1060 <sbx_user1060@ip-10-129-10-165.eu-west-2.compute.internal>
2019-02-23 12:52:36 +0000

    Sat Feb 23 12:52:36 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,16 +1,10 @@
-
-
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-#    if type(friend_name) == type(str()):
-#        raise TypeError
+    if not isinstance(friend_name, str):
+        raise TypeError
     return "Hello, " + friend_name + "!"
 
-
-hello("Matto")
-
-#hello(1)
 
 
 
