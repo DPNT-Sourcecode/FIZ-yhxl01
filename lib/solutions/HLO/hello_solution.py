b17f5e1a374ab90c70302303871d754139aed7db
sbx_user1060 <sbx_user1060@ip-10-129-10-165.eu-west-2.compute.internal>
2019-02-23 12:53:21 +0000

    Sat Feb 23 12:53:21 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,9 +1,8 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    if not isinstance(friend_name, str):
-        raise TypeError
     return "Hello, " + friend_name + "!"
+
 
 
 
