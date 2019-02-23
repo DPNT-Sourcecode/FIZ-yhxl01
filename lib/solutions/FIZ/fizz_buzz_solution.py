071ae7d03aa88c1afecb6bbd08f171471c8a06f7
sbx_user1060 <sbx_user1060@ip-10-129-10-165.eu-west-2.compute.internal>
2019-02-23 12:59:35 +0000

    Sat Feb 23 12:59:35 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,3 +1,14 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    raise NotImplementedError()
+
+    if number % 3 == 0 and number % 5 == 0:
+        return "fizz buzz"
+    
+    if number % 3 == 0:
+        return "fizz"
+
+    if number % 5 == 0: 
+        return "buzz"
+
+    return str(number)
+
