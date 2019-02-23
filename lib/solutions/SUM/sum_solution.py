1f89c56e5660853f640bcc1e6136195659527db6
sbx_user1060 <sbx_user1060@ip-10-129-10-165.eu-west-2.compute.internal>
2019-02-23 12:35:33 +0000

    Sat Feb 23 12:35:33 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,38 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    """Add two integers, both with value between 0-100
+
+    :param intA: First integer to add
+    :type intA: int
+    :param intB: Second integer to add
+    :type intB: int
+    :return: The sum of the parameters
+    :rtype: int
+    """
+    assert (intA >= 0), "Negative values out of bounds"
+    assert (intB >= 0), "Negative values out of bounds"
+    assert (intA <= 100), "Max allowed value 100"
+    assert (intB <= 100), "Max allowed value 100"
+
+    return intA + intB
+
+
+def test_compute_basic():
+    """Basic test of compute function
+    """
+    assert compute(10, 10) == 20
+
+
+def test_compute_negative():
+    """Test that negative input raises AssertionError
+    """
+    with pytest.raises(AssertionError):
+        compute(-1,1)
+
+
+def test_compute_gt100():
+    """Test that input over 100 raises AssertionError
+    """
+    with pytest.raises(AssertionError):
+        compute(101, 1)
+
