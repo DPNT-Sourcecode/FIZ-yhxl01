ab099869a225f025e63d0db39fd04ac0900b088d
sbx_user1060 <sbx_user1060@ip-10-129-10-165.eu-west-2.compute.internal>
2019-02-23 13:01:02 +0000

    Sat Feb 23 13:01:02 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -12,3 +12,19 @@ def fizz_buzz(number):
 
     return str(number)
 
+def test_fizzbuzz():
+    assert(fizz_buzz(15), "fizz buzz")
+
+
+def test_fizz():
+    assert(fizz_buzz(6), "fizz")
+
+
+def test_buzz():
+    assert(fizz_buzz(10), "buzz")
+
+
+def test_other():
+    assert(fizz_buzz(2), "2")
+
+
