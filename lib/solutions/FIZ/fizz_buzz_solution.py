bda323adcb299d16052969ed5a3dd06e52e17d9d
sbx_user1060 <sbx_user1060@ip-10-129-10-165.eu-west-2.compute.internal>
2019-02-23 13:18:27 +0000

    Sat Feb 23 13:18:27 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -3,12 +3,16 @@ def fizz_buzz(number):
 
     is_fizz = False
     is_buzz = False
+    is_delux = False
 
     if number % 3 == 0 or "3" in str(number):
         is_fizz = True
     
     if number % 5 == 0 or "5" in str(number):
         is_buzz = True
+
+    if number > 10 and len(set(str(number))) == 1:
+        is_delux = True
 
     if is_fizz and is_buzz:
         return "fizz buzz"
@@ -32,5 +36,13 @@ def test_buzz():
     assert(fizz_buzz(10), "buzz")
 
 
+def test_delux():
+    assert(fizz_buzz(222), "delux")
+
+
+def test_fizz_buzz_delux():
+    assert(fizz_buzz(555), "fizz buzz delux")
+
 def test_other():
     assert(fizz_buzz(2), "2")
+
