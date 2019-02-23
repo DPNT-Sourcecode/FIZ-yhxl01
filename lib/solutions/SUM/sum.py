e16a4cbad7d74dff9e2fe85429f118ae2a8cd866
sbx_user1060 <sbx_user1060@ip-10-129-10-165.eu-west-2.compute.internal>
2019-02-23 12:12:04 +0000

    Sat Feb 23 12:12:04 UTC 2019

---
+++ lib/solutions/SUM/sum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+def sum(intA, intB):
+    """Add two integers, both with value between 0-100
+
+    :param intA: First integer to add
+    :type intA: int
+    :param intB: Second integer to add
+    :type intB: int
+    :return: The sum of the parameters
+    :rtype: int
+    """
+    assert (intA >= 0), "Negative values out of bounds"
+    assert (intB >= 0), "Negative values out of bounds"
+    assert (intA <= 100), "Max allowed value 100"
+    assert (intB <= 100), "Max allowed value 100"
+
+    return intA + intB
